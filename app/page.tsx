ddf51740b36ca3f59bee3321ca225c5ebe3eba76
Jorge <jorgerocha_10@hotmail.com>
2025-03-04 21:20:51 -0700

    2.2 Seed database with sample data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { PrismaClient } from '@prisma/client'
 import type { Location, FrameStyle, Size, Layer, Map } from '@prisma/client'
 
 const prisma = new PrismaClient();
